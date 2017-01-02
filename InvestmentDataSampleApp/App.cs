f31ebb9d71a282d08bac95fc9d99ab6a811dc2ad
brminnick <brandon.r.minnick@gmail.com>
2017-01-01 16:16:19 -0800

    Refactored Folder Structure

---
+++ InvestmentDataSampleApp/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace InvestmentDataSampleApp
+{
+	public class App : Application
+	{
+		static OpportunityModelDatabase _database;
+
+		public App()
+		{
+            var customNavigationPage = new ShakeListenerNavigationPage(new OpportunitiesPage());
+
+            MainPage = customNavigationPage;
+		}
+
+		public static OpportunityModelDatabase Database=>
+			_database ?? (_database = new OpportunityModelDatabase());
+	}
+}
+
