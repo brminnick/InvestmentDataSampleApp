de45369406787173906cff38fcd7a9062520d31e
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-04-18 17:27:59 -0700

    Updated NuGet Packages

--- Src/InvestmentDataSampleApp.UITests/Tests/ReplTests.cs
+++ Src/InvestmentDataSampleApp.UITests/Tests/ReplTests.cs
@@ -4,21 +4,17 @@ using Xamarin.UITest;
 
 namespace InvestmentDataSampleApp.UITests
 {
-	[TestFixture(Platform.Android)]
-	[TestFixture(Platform.iOS)]
-	public class ReplTests : BaseTest
-	{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class ReplTests : BaseTest
+    {
 
-		public ReplTests(Platform platform) : base(platform)
-		{
-		}
+        public ReplTests(Platform platform) : base(platform)
+        {
+        }
 
-		[Ignore]
-		[Test]
-		public void ReplTest()
-		{
-			App.Repl();
-		}
-	}
+        [Test, Ignore("REPL used for manually exploring app")]
+        public void ReplTest() => App.Repl();
+    }
 }
 
